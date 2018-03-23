e4186c4e8dfe97bdee84552d2e7520a86c7129a7
LochTechMonster <36462628+LochTechMonster@users.noreply.github.com>
2018-03-23 18:42:15 +1100

    got the slideshow sort of

---
+++ js/slider-initialise.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+$(document).ready(function(){
+	'use strict';
+  $('.slideshow').slick({
+    infinite:true,
+	fade:true,
+	speed:900,
+	autoplay:true,
+	autoplaySpeed:4000,
+	arrows:false,
+  });
+});
